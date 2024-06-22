a4b393effb61c76a5e44039f2bb73ea09e988a85
Paras Naulia <parasnaulia645@gmail.com>
2024-06-22 15:49:24 +0530

    first commit 2

--- src/Db/Conn.js
+++ src/Db/Conn.js
@@ -3,11 +3,7 @@ const mongoose = require("mongoose");
 (async function connectToDatabase() {
   try {
     await mongoose.connect(
-      "mongodb+srv://parasnaulia645:nx7w2cQ4pHnjfTbl@cluster0.ivdbulp.mongodb.net/parasnaulia645",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
+      "mongodb+srv://parasnaulia645:nx7w2cQ4pHnjfTbl@cluster0.ivdbulp.mongodb.net/parasnaulia645"
     );
     console.log("Connection is successful");
   } catch (e) {
